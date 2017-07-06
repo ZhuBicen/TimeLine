ade6af52797c05a5ad84e111aad3706b4601886b
bzhu <zhu.bicen@gmail.com>
2017-07-06 11:10:41 +0800

    Serve home properly

--- TimeChart.go
+++ TimeChart.go
@@ -5,9 +5,17 @@ import (
 	"net/http"
 )
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "TimeChart.html")
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+
+	http.HandleFunc("/", handleHome)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
